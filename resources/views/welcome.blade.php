9b01beb74ec93ce6fea73aea6db193835aa535a9
cyberpunk3351 <drummer.ilya@gmail.com>
2021-05-19 15:15:49 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -17,15 +18,85 @@
             }
         </style>
     </head>
-    <body>
+    <body class="container">
         <x-chart canvas="canvas1" :name="['a', 'b', 'c']" :weight="['617594', '181045', '281045']"/>
+        <div id="message" x-data="{ showMessage: false, message: '' }">
+            <div x-show="showMessage">
+                <p class="text-sm leading-5 font-medium text-gray-900" x-text="message">
+                </p>
+            </div>
+        </div>
+
+        <div class="container">
+            <canvas id="canvas2"></canvas>
+        </div>
+        
+        <script>
+            massPopChart = new Chart(canvas2, {
+            type: 'bar', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+            data: {
+                labels: ['a', 'b', 'c'],
+                datasets: [{
+                    label: 'Запонение',
+                    data: ['617594', '181045', '281045'],
+                    //backgroundColor:'green',
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)'
+                    ],
+                    borderWidth: 1,
+                    borderColor: '#777',
+                    hoverBorderWidth: 3,
+                    hoverBorderColor: '#000'
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Заполнение бочек углем',
+                    fontSize: 25
+                },
+                legend: {
+                    display: true,
+                    position: 'right',
+                    labels: {
+                        fontColor: '#000'
+                    }
+                },
+                layout: {
+                    padding: {
+                        left: 50,
+                        right: 0,
+                        bottom: 0,
+                        top: 0
+                    }
+                },
+                tooltips: {
+                    enabled: true
+                }
+            }
+        });
+        </script>
+        
+
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
         <script>
             Echo.channel('events')
-                .listen('RealTimeMessage', (e) => console.log('RealTimeMessage: ' + e.message));
+            //     .listen('RealTimeMessage', (e) => console.log('RealTimeMessage: ' + e.message));
+            .listen('RealTimeMessage', (e) => {
+                let message = document.getElementById('message');
+                message.__x.$data.showMessage = true;
+                message.__x.$data.message = e.message;
+            });     
+                
         </script>
     </body>
 </html>
